730d0c0848e2a0e11212edc96dfd3fdbc3c03aee
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-28 00:30:52 +0300

    brandsListed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project';
+  title:string = 'CarRental';
+  user:string ="Melik Firat Basci"; 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { BrandComponent } from './components/brand/brand.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
+    BrandComponent,
     
   ],
   imports: [
