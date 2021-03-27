2858de8c2a5f58c54d4e13d94286fb5f156a0455
Melik fırat Başçi <mbasci23@hotmail.com>
2021-03-28 00:58:10 +0300

    colorsListed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { BrandComponent } from './components/brand/brand.component';
     AppComponent,
     CustomerComponent,
     BrandComponent,
+    ColorComponent,
     
   ],
   imports: [
